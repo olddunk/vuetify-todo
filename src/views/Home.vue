d395c3648d251533a42a771fb484608150950a79
olddunk <yegc22@gmail.com>
2022-01-25 19:22:05 +0800

    Project begins

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,9 @@
 <template>
 <div class="home">
-  <h1>Homepage</h1>
-  <p class="pink lighten-4 red--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima reiciendis amet porro rem? Architecto, eligendi? Necessitatibus sunt nam facilis dignissimos.</p>
-  <p class="indigo lighten-5 indigo--text text--darken-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo voluptas voluptate minima sapiente ratione similique, impedit nostrum totam! Corrupti, vero?</p>
-  <h1 class="text-h4">Massive Display</h1>
 </div>
 </template>
 
 <script>
-
   export default {
     name: 'Home',
   }
